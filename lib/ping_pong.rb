fdab6823359ed5c2420c250eeeed0c797e4c8758
Andrew Lee & Jennifer Bolanos <clact2013@hotmail.com>
2015-04-20 12:07:48 -0700

    Final ping pong program. works

--- lib/ping_pong.rb
+++ lib/ping_pong.rb
@@ -3,8 +3,12 @@ class Fixnum
     new_array = Array (1..self)
     pp_array = []
     new_array.each() do |number|
-      if number.%(3).eql?(0)
+      if number.%(3).eql?(0).&(number.%(5).eql?(0))
+        pp_array.push("ping-pong")
+      elsif number.%(3).eql?(0)
         pp_array.push("ping")
+      elsif number.%(5).eql?(0)
+        pp_array.push("pong")
       else
         pp_array.push(number)
       end
--- spec/ping_pong_spec.rb
+++ spec/ping_pong_spec.rb
@@ -10,4 +10,11 @@ describe('Fixnum#ping_pong') do
     expect((3).ping_pong()).to(eq([1,2,"ping"]))
   end
 
+  it("if it is divisible by 5, return pong") do
+    expect((5).ping_pong()).to(eq([1,2,"ping",4,"pong"]))
+  end
+
+  it("if it is divisible by 3 and 5, return ping pong") do
+    expect((15).ping_pong()).to(eq([1,2,"ping",4,"pong","ping",7,8,"ping","pong",11,"ping",13,14,"ping-pong"]))
+  end
 end
